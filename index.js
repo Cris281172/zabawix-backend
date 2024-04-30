76ff45410eaa041e9c754a7a26be6c4769b7b0a8
Cris281172 <krzysiu.juczynski@gmail.com>
2024-04-30 15:51:34 +0200

    Update

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const uploadFile = require('./utils/uploadFile')
+const uploadFile = require('./utils/uploadFile');
 const saveVisit = require('./middleware/saveVisitMiddleware');
-const app = express()
+const app = express();
 // uploadFile('test.png', 'test.png').catch(console.error);
 const mainRoutes = require('./routes/main');
 const bodyParser = require('body-parser');
 const https = require("https");
-const cors = require('cors')
-app.use(cors())
-require('dotenv').config()
-app.use(bodyParser.json())
-app.use('/', mainRoutes)
+require('dotenv').config();
 
+// Definiowanie własnych nagłówków CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");  // Zmień '*' na konkretną domenę, jeśli potrzebujesz większego bezpieczeństwa
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
 
+app.use(bodyParser.json());
+app.use('/', mainRoutes);
 
 mongoose.connect(process.env.DB_URL).then(() => {
     console.log('Connected to DB');
-})
-    .catch(err => {
-        console.log(err)
-    })
+}).catch(err => {
+    console.log(err);
+});
 
-// https.createServer({}, app)
-//     .listen(8443, () => {
-//         console.log('Server listen on 8080 port')
-//     })
-
-app.listen(8080, () => console.log(`Server listening on ${process.env.PORT} port`))
+app.listen(process.env.PORT || 8080, () => console.log(`Server listening on ${process.env.PORT || 8080} port`));
