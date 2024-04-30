197d78beea7a52ccb6316d5dbbb3d4570ac3e54f
Cris281172 <krzysiu.juczynski@gmail.com>
2024-04-30 15:38:38 +0200

    Update

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ const app = express()
 const mainRoutes = require('./routes/main');
 const bodyParser = require('body-parser');
 const https = require("https");
-const cors = require('cors')({origin: 'https://zabawix.com/'})
+const cors = require('cors')({origin: true})
 require('dotenv').config()
-app.use(cors());
+app.use(cors);
 app.use(bodyParser.json())
 app.use('/', mainRoutes)
 
 
-
-mongoose.connect(process.env.DB_URL).then(() => {
-    console.log('Connected to DB');
-})
-    .catch(err => {
-        console.log(err)
-    })
-
-// https.createServer({}, app)
-//     .listen(8443, () => {
-//         console.log('Server listen on 8080 port')
+//
+// mongoose.connect(process.env.DB_URL).then(() => {
+//     console.log('Connected to DB');
+// })
+//     .catch(err => {
+//         console.log(err)
 //     })
+
+https.createServer({}, app)
+    .listen(8443, () => {
+        console.log('Server listen on 8080 port')
+    })
 
 app.listen(8080, () => console.log(`Server listening on ${process.env.PORT} port`))
