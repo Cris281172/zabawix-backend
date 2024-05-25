4a3cb23b8516ab429da9835199b9426777808f0a
Cris281172 <krzysiu.juczynski@gmail.com>
2024-05-25 11:19:18 +0200

    Update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 const uploadFile = require('./utils/uploadFile')
 const saveVisit = require('./middleware/saveVisitMiddleware');
 const app = express()
