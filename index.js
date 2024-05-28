064b45ed209012488730de1ab4562bfb10fa84ac
Cris281172 <krzysiu.juczynski@gmail.com>
2024-05-28 17:14:03 +0200

    Update

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ require('dotenv').config()
 app.use(bodyParser.json({
     limit: '100mb'
 }))
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
+
 app.use('/', mainRoutes)
 
 mongoose.connect(process.env.DB_URL).then(() => {
